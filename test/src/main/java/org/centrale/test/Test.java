6f299de48522a4dceec9a4ca889078b2f82683ec
Arthur Chereau <arthur.chereau06@gmail.com>
2024-01-15 14:32:05 +0100

    Matthieu jtm <3

---
+++ test/src/main/java/org/centrale/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.test;
+
+/**
+ *
+ * @author arthu
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
