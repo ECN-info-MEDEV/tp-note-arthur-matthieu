54fff851ef3dad2009ac63e55395df7afe20b69e
Arthur Chereau <arthur.chereau06@gmail.com>
2024-01-18 10:20:52 +0100

    Update GrilleTest.java

--- src/test/java/org/centrale/test/GrilleTest.java
+++ src/test/java/org/centrale/test/GrilleTest.java
@@ -14,5 +14,63 @@ import static org.junit.Assert.*;
  * @author arthu
  */
 public class GrilleTest {
+    
+    public GrilleTest() {
     }
-   
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getTaille method, of class Grille.
+     */
+    @Test
+    public void testGetTaille() {
+        Grille g = new Grille(5);
+        assertEquals(5, g.getTaille());
+    }
+
+    /**
+     * Test of setTaille method, of class Grille.
+     */
+    @Test
+    public void testSetTaille() {
+        Grille g = new Grille(5);
+        g.setTaille(7);
+        assertEquals(7, g.getTaille());
+    }
+    
+    /**
+     * Test of getGrille method, of class Grille.
+     */
+    @Test
+    public void testGetGrille() {
+                
+                Grille g = new Grille(5);
+
+                // Testez le getter initial
+                assertNotNull(g.getGrille());
+    }
+
+    /**
+     * Test of setGrille method, of class Grille.
+     */
+    @Test
+    public void testSetGrille() {
+            Grille g = new Grille (5); 
+                
+                char[][] newGrid = new char[5][5];
+                newGrid[2][2] = 'B';
+        
+                
+                g.setGrille(newGrid);
+        
+                
+                assertSame(newGrid, g.getGrille());
+    }
+}
