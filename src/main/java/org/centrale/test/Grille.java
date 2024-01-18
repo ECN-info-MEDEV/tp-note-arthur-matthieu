29889afe99c8cc95d3a7914cb777d638d04e5974
MayBMatt <matt.bournat@gmail.com>
2024-01-18 09:04:34 +0100

    Classe Grille v1

--- src/main/java/org/centrale/test/Grille.java
+++ src/main/java/org/centrale/test/Grille.java
@@ -4,10 +4,120 @@
  */
 package org.centrale.test;
 
+
 /**
  *
  * @author mattb
  */
 public class Grille {
     
+    private int taille;
+    private char[][] grille;
+
+    /**
+     *
+     * @param taille
+     */
+    public Grille(int taille) {
+        this.taille = taille;
+        this.grille = new char[taille][taille];
+        for (int i =0; i < taille ; i++){
+            
+            for (int j = 0 ; j < taille ; j++){
+                
+                this.grille[i][j] = '.' ; 
+                
+            }
+            
+        }
+        
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getTaille() {
+        return taille;
+    }
+
+    /**
+     *
+     * @param taille
+     */
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public char[][] getGrille() {
+        return grille;
+    }
+
+    /**
+     *
+     * @param grille
+     */
+    public void setGrille(char[][] grille) {
+        this.grille = grille;
+    }
+    
+    
+    private void placeBateau(int x , int y){
+        this.grille[x][y]='B';
+    }
+    
+    private void touche(int x, int y){
+        
+        if (this.grille[x][y] == 'B'){
+            
+            this.grille[x][y] = 'O';
+            
+        } 
+        
+        else {
+            
+            this.grille[x][y] = 'X';
+            
+        }
+    }
+    
+    private void affiche (){
+        for (int i =0; i < taille ; i++){
+            
+            for (int j = 0 ; j < taille ; j++){
+                
+                System.out.print(this.grille[i][j]+' '); 
+                
+            }
+            
+            System.out.println();
+        }
+    }
+    
+    private void afficheAdversaire (){
+        
+        for (int i =0; i < taille ; i++){
+            
+            for (int j = 0 ; j < taille ; j++){
+                
+                if(this.grille[i][j] == 'B'){
+                    
+                    System.out.print('.'+' ');
+                    
+                }
+                
+                else{
+                    
+                    System.out.print(this.grille[i][j]+' ');
+                    
+                }
+            }
+            
+            System.out.println();
+        }
+    }
 }
