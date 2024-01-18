3c8b899df31e202af2ddedd8b7794382b471b115
MayBMatt <matt.bournat@gmail.com>
2024-01-18 08:29:02 +0100

    Création des classes

--- src/main/java/org/centrale/test/Grille.java
+++ src/main/java/org/centrale/test/Grille.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package org.centrale.test;
 
 /**
  *
- * @author arthu
+ * @author mattb
  */
-public class Test {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Grille {
+    
 }
